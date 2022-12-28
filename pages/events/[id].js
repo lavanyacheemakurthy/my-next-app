453a9f94ac254cd0576dd07d90da13e901709dab
lavanyacheemakurthy <chslavanya@yahoo.com>
2022-12-28 14:43:33 +0530

    first commit

---
+++ pages/events/[id].js
@@ -0,0 +1,6 @@
+const Page =()=>{
+    return <div>
+<h1>Our Single Event page</h1>
+    </div>
+}
+export default Page;
---
+++ pages/events/index.js
@@ -0,0 +1,20 @@
+export default function Events() {
+    return <div>
+      <h1>Events Page</h1>
+          <a href="">
+            <img />
+            <h2>Events in London</h2>
+            <p>
+              
+            </p>
+          </a>
+          <a href="">
+            <img />
+            <h2>Events in Barcelona</h2>
+            <p>
+              
+            </p>
+          </a>
+       
+    </div>
+  }
